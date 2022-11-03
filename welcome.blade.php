aa318ffbd84616c1633c5a29a71a7588d51a1546
40023612 <94069784+40023612@users.noreply.github.com>
2022-11-03 15:53:11 +0000

    made code changes

---
+++ welcome.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.layout')
+
+@section('content')
+        <nav id = "navArea"> 
+            <div><img src="url" alt="alternatetext">
+            </div>
+            <div><form action="" method="get">
+                <label for="Search">Search:</label>
+                <input type="text" name="Search" id="Search">
+            </form></div>
+            <div><h3>sign in</h3></div>
+            <div><i class="fa fa-shopping-cart"></i></div>
+        </nav>
+        <div class="thumbNailArea">
+            <div><img class="image-holders" src="/img/CMEchristmas.png" alt="alternatetext"></div>
+            <div><img class="image-holders" src="/img/CMERings.JPG" alt="alternatetext"></div>
+            <div><img class="image-holders" src="/img/CMEEarringsPendantBraceletRing.jpg" alt="alternatetext"></div>
+        </div>
+
+        <div id = "itemsForSale">
+            <div class = "item">1</div>
+            <div class = "item">2</div>
+            <div class = "item">3</div>
+            <div class = "item">4</div>
+
+        </div>
+        
+        <script src="" async defer></script>
+@endsection
+       
